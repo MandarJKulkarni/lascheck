9c13273989526489982eb128f800ef33baae5cb3
mandarjkulkarni <mjkool@gmail.com>
2020-08-30 21:24:23 +0530

    added a todo

--- tests/test_conformity.py
+++ tests/test_conformity.py
@@ -12,8 +12,10 @@ readfromexamples = lambda fn: os.path.join(os.path.dirname(__file__), "examples"
 
 logger = logging.getLogger(__name__)
 
+# todo: add test for file missing multiple sections
+# todo: add test for missing_a_section.las
 
-# todo: add test for missing_a_section
+
 def test_check_conforming_no_version_section():
     las = lascheck.read(readfromexamples("missing_version_section.las"))
     assert not las.check_conformity()
